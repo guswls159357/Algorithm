dfab8e0198bb5b71cc1f00748adb4ee81374d855
guswls159357 <67905075+guswls159357@users.noreply.github.com>
2023-03-16 22:04:51 +0900

    [level 2] Title: 스킬트리, Time: 0.16 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,28 +1,53 @@
 import java.util.*;
 
 class Solution {
+    
+    // 현재 스킬들을 검색하기 위한 용도
+    HashSet<Character> skillSet = new HashSet();
+    // 현재 스킬들을 순서대로 체크하기 위한 용도
+    char[] skills;
+    
     public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-
-        for(int i = 0; i < skill_trees.length; i++){
-            String skill_tree = skill_trees[i];
-            StringBuilder sb = new StringBuilder();
-
-            for(int j = 0; j < skill_tree.length(); j++){
-                if(skill.contains(String.valueOf(skill_tree.charAt(j)))){
-                    sb.append(skill_tree.charAt(j));
-                }
-            }
-
-            for(int j = skill.length(); j > -1; j--){
-                String s = skill.substring(0, j);
-                if(s.equals(sb.toString())){
-                    answer++;
-                    break;
-                }
-            }
-
+        
+        int result = 0;
+        init(skill);
+        
+        for(String skillTree : skill_trees){
+            if(isPossible(skillTree)) result++;
         }
-        return answer;
+        
+        return result;
+    }
+    
+    public boolean isPossible(String skillTree){
+        
+        int length = skillTree.length();
+        int curSkillIdx = 0;
+        char curC;
+        for(int i = 0 ; i < length ; i++){
+            curC = skillTree.charAt(i);
+            
+            if(skillSet.contains(curC)){
+                if(skills[curSkillIdx] == curC){
+                    curSkillIdx++;
+                }else{
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public void init(String skill){
+        int length = skill.length();
+        skills = new char[length];
+        
+        char c;
+        for(int i = 0 ; i < length ; i++){
+            c = skill.charAt(i);
+            skills[i] = c;
+            skillSet.add(c);
+        }
     }
 }
