64ef61a1f0bbd72516ef157c654cb298017e0ea5
guswls159357 <67905075+guswls159357@users.noreply.github.com>
2023-04-05 15:03:30 +0900

    [Silver I] Title: 부분수열의 합, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -5,33 +5,23 @@ public class Main {
 
     static int N;
     static int[] arr;
-    static Set<Integer> sumSet = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
 
         initInput();
-        dfs(0, 0);
-        System.out.println(getMinImpossible());
+        System.out.println(getAns());
     }
 
-    static int getMinImpossible(){
+    static int getAns() {
+
         int target = 1;
-        while(sumSet.contains(target)){
-            target++;
+
+        for (int i = 0; i < N; i++) {
+            if (target < arr[i]) break;
+            target += arr[i];
         }
+
         return target;
-    }
-
-    static void dfs(int depth, int sum){
-
-        if(depth == N){
-            sumSet.add(sum);
-            return;
-        }
-
-        // 선택할지, 선택하지 않을지
-        dfs(depth + 1, sum);
-        dfs(depth + 1, sum + arr[depth]);
     }
 
     static void initInput() throws IOException {
@@ -40,9 +30,11 @@ public class Main {
         arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0 ; i < N ; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr);
 
         br.close();
     }
