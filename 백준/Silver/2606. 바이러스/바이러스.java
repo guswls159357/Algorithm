e600607dd60c726134f600425103eeb28f6a9c34
guswls159357 <67905075+guswls159357@users.noreply.github.com>
2023-04-05 22:19:10 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,56 +1,53 @@
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N; // 컴퓨터의 수
+    static int M; // 간선 갯수
+    static int count; // 1 번 컴퓨터로 감염되는 컴퓨터의 수
+    static boolean[][] connectedMap; // 연결 상태 배열
+    static boolean[] visited; // 방문 배열
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int cnt = Integer.parseInt(br.readLine());
-
-        int N = Integer.parseInt(br.readLine());
-
-        boolean map[][] = new boolean[cnt+1][cnt+1];
-
-        StringTokenizer st;
-        int tmp1;
-        int tmp2;
-
-        for(int i = 0 ; i < N ; i++){
-            st = new StringTokenizer(br.readLine());
-            tmp1 = Integer.parseInt(st.nextToken());
-            tmp2 = Integer.parseInt(st.nextToken());
-
-            map[tmp1][tmp2] = true;
-            map[tmp2][tmp1] = true;
-        }
-
-        boolean visited[] = new boolean[cnt+1];
-        Queue<Integer> queue = new LinkedList<>();
-
-        queue.add(1);
-        visited[1] = true;
-
-        while(!queue.isEmpty()){
-
-            int num = queue.poll();
-
-            for(int i = 1 ; i <= cnt ; i++){
-                if(map[num][i] && !visited[i]) {
-                    visited[i] = true;
-                    queue.add(i);
-                }
-            }
-        }
-
-        int result = 0;
-        for(int i = 2 ; i <= cnt ; i++){
-            if(visited[i]) result++;
-        }
-
-        System.out.println(result);
+        initInput();
+        dfs(1);
+        System.out.println(count - 1);
     }
 
+    static void dfs(int start) {
+
+        visited[start] = true;
+        count++;
+
+        for (int i = 2; i <= N; i++) {
+            if (connectedMap[start][i] && !visited[i]) {
+                visited[i] = true;
+                dfs(i);
+            }
+        }
+    }
+
+    static void initInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        connectedMap = new boolean[N + 1][N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int com1 = Integer.parseInt(st.nextToken());
+            int com2 = Integer.parseInt(st.nextToken());
+
+            connectedMap[com1][com2] = true;
+            connectedMap[com2][com1] = true;
+        }
+
+        br.close();
+    }
 }
