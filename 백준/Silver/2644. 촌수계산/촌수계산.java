8bff11da2f4d7094e02b23bc2e246ddebd848db4
guswls159357 <67905075+guswls159357@users.noreply.github.com>
2023-04-10 22:16:16 +0900

    [Silver II] Title: 촌수계산, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -3,34 +3,38 @@ import java.util.*;
 
 public class Main {
 
+    // N - 총 사람 수 , M - 부모 자식들 간의 관계의 갯수
     static int N, M;
-    static int num1, num2;
-    static int answer = -1;
+    // x - 시작 , y - 끝
+    static int x, y;
     static boolean[][] connected;
     static boolean[] visited;
+    // 촌수를 계산할 수 없다면 -1
+    static int answer = -1;
 
     public static void main(String[] args) throws IOException {
 
         initInput();
-        dfs(num1, 0);
+        dfs(x, 0);
         System.out.println(answer);
     }
 
     static void dfs(int start, int depth) {
 
-        visited[start] = true;
-        //System.out.println(start);
-
-        if (start == num2) {
+        // 탈출 조건
+        if (start == y) {
             answer = depth;
             return;
         }
 
+        // 방문처리
+        visited[start] = true;
+
+        // 부모 자식 관계인 사람들만 재귀 호출
         for (int i = 1; i <= N; i++) {
-            if (i != start && !visited[i] && connected[start][i]) {
-                dfs(i, depth + 1);
-            }
+            if (!visited[i] && connected[start][i]) dfs(i, depth + 1);
         }
+
     }
 
     static void initInput() throws IOException {
@@ -40,11 +44,10 @@ public class Main {
         visited = new boolean[N + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        num1 = Integer.parseInt(st.nextToken());
-        num2 = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
 
         M = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int p1 = Integer.parseInt(st.nextToken());
